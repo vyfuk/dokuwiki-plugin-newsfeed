a2e6ce6f7835787abbd2e97181bd226b93a9997e
syselkk <k.kolar@email.cz>
2015-09-14 19:10:46 +0200

    pokus o překlad dokuwiki-plugin-fksnewsfeed
    Pokud se nelíbí překlady "stream -> vlákno", "rodič -> matka" a "díte -> dcera", tak mi napište, jak byste to napsali lépe...

--- lang/cs/lang.php
+++ lang/cs/lang.php
@@ -10,9 +10,9 @@ $lang['direction']  = 'ltr';
 /**
  * other buttons
  */
-$lang['btn_edit_news']      = 'Upraviť túto novinku';
-$lang['btn_more_news']      = 'Staršie novinky';
-$lang['btn_newsfeed_link']  = 'Link na túto novinku';
+$lang['btn_edit_news']      = 'Upravit tuto novinku';
+$lang['btn_more_news']      = 'Starší novinky';
+$lang['btn_newsfeed_link']  = 'Link na tuto novinku';
 /**
  * label to form
  */
@@ -23,7 +23,7 @@ $lang['newsdate']         = 'Datum';
 $lang['newsname']         = 'Nadpis';
 $lang['name']             = 'Nadpis';
 $lang['image']            = 'Obrázek';
-$lang['category']         = 'Kategória';
+$lang['category']         = 'Kategorie';
 $lang['weight']           = 'Váha';
 
 
@@ -46,57 +46,57 @@ $lang['dec']              = 'prosince';
 /**
  * Waring & info
  */
-$lang['no_more']           ='Niesu žiadné dalšie aktuality na zobrazenie';
+$lang['no_more']           ='Momentálně nejsou žádné další aktuality';
 $lang['news_non_exist'] = 'Ops!';
 /**
  * @TODO vymyslieť niečo pekné!
  */
-$lang['o_rly_delete'] = 'O RLY: Chceš zmazat túto aktualitu?';
+$lang['o_rly_delete'] = 'O RLY: Chceš smazat tuto aktualitu?';
 $lang['js']['oRlyDelete'] = $lang['o_rly_delete'];
 /**
  * to create news
  */
-$lang['menu_create_news'] = 'Vytvoriť novinku';
+$lang['menu_create_news'] = 'Vytvořit novinku';
 $lang['btn_create_news'] = $lang['menu_create_news'] ;
 $lang['info_create_news'] ='';
 /**
  * manage stream
  */
-$lang['menu_manage_stream'] = 'Spravovať stream';
+$lang['menu_manage_stream'] = 'Spravovat vlákno (najdeš tam i vytvoření novinky)';
 $lang['btn_manage_stream'] = $lang['menu_manage_stream'];
-$lang['info_manage_news'] = 'Spravovať tento stream:';
+$lang['info_manage_news'] = 'Spravovat toto vlákno:';
 /**
  * add_to_stream
  */
-$lang['menu_add_to_stream']='Pridať novinku do streamu';
+$lang['menu_add_to_stream']='Přidat novinku do vlákna';
 $lang['btn_add_to_stream'] = $lang['menu_add_to_stream'];
 $lang['info_add_to_stream'] = '';
 /**
  * change order
  */
-$lang['menu_change_order'] = 'Upravit pořadí alebo zmazať novinku';
-$lang['btn_change_order'] = 'Zmeniť pořadí';
+$lang['menu_change_order'] = 'Upravit pořadí a/nebo smazat novinku';
+$lang['btn_change_order'] = 'Změnit pořadí';
 $lang['info_change_order'] = '';
 /**
  * for admin dependence
  */
-$lang['dep_menu']='Upravit závyslosti streamov';
-$lang['dep_create']='Vytvoriť novú závyslosť';
-$lang['dep_created']='Závysloť bola vytvorená';
-$lang['dep_exist']='závysloť už existuje';
-$lang['dep_full_info']='Novinka pridaná do rodičovského streamu sa automaticky pridá aj do detského streamu. <br />
-                Novinka pridaná do detského streamu sa v rodičovskom nezobrazí.';
-$lang['dep_parent_info']='Stream od ktorého sa dedí "rodič"';
-$lang['dep_child_info']='Stream do ktorého sa dedí "dieťa"';
-$lang['dep_list']='Zoznam Streamov a ich závyslostí';
-$lang['dep_list_parent']='Od tohoto streamu priamo dedí, je rodič voči:';
-$lang['dep_list_child']='Do tohoto streamu priamo dedí, je dieťa voči:';
-$lang['dep_list_parent_full']='Od tohoto streamu celkovo dedí, je (pra)rodič voči:';
-$lang['dep_list_child_full']='Do tohoto streamu celkovo dedí, je (pra)dieťa voči:';
+$lang['dep_menu']='Upravit závislosti vláken';
+$lang['dep_create']='Vytvořit novú závislost';
+$lang['dep_created']='Závislost byla vytvoření';
+$lang['dep_exist']='Závislost už existuje';
+$lang['dep_full_info']='Novinka přidaná do mateřského vlákna se automaticky přidá i do dceřiného vlákna. <br />
+                Novinka přidaná do dceřiného vlákna se v mateřském nezobrazí.';
+$lang['dep_parent_info']='Vlákno od kterého se dědí "matka"';
+$lang['dep_child_info']='Vlákno do kterého se dědí "dcera"';
+$lang['dep_list']='Seznam vláken a jejich závislostí';
+$lang['dep_list_parent']='Od tohoto vlákna přímo dědí, je matka voči:';
+$lang['dep_list_child']='Do tohoto vlákna přímo dědí, je dcera voči:';
+$lang['dep_list_parent_full']='Od tohoto vlákna celkovo dědí, je matka/babička vůči:';
+$lang['dep_list_child_full']='Do tohoto vlákna celkovo dědí, je dcera/vnučka vůči:';
 
 /**
  * for admin stream
  */
-$lang['stream_create']='Create stream';
-$lang['stream_list']='List of streams';
-$lang['stream_delete']='Remove stream';
+$lang['stream_create']='Vytvoř vlákno';
+$lang['stream_list']='Seznam vláken';
+$lang['stream_delete']='Odstraň vlákno';
